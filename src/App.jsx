3b1eacd989b2681d684e6d457a99c9b38ffda003
Brandon Castillo <brandoncastillo.09@gmail.com>
2025-09-16 10:53:44 -0300

    ADD: multicolor title

--- src/App.jsx
+++ src/App.jsx
@@ -3,11 +3,27 @@ import './styles/app.css'
 
 function App() {
   const [count, setCount] = useState(0)
+  const usuario = "Brandon"
+  const [dark, setDark] = useState(false)
+  const [eng, setEng] = useState(false)
+
+  function handleDarkMode(){
+    setDark(!dark)
+  }
 
   return (
-    <>
-        <h1>On the level</h1>      
-    </>
+    <main className={dark ? "main-dark" : "main-light"}>
+        <h1><span>T</span><span>o</span> <span>D</span><span>o</span> <span>L</span><span>i</span><span>s</span><span>t</span></h1>
+
+        <h2>Buen día {usuario},<span className='second-line'>
+          <span>¿</span><span>Q</span><span>u</span><span>é</span> <span>d</span><span>e</span><span>s</span><span>e</span>a <span>h</span><span>a</span><span>c</span><span>e</span><span>r</span> <span>h</span><span>o</span><span>y</span>?</span> </h2>
+          {/* ¿Qué desea hacer hoy?</span></h2> */}
+        <input ty>
+        
+        </input>
+
+        <button onClick={()=>handleDarkMode()}> {dark? "Modo Claro" : "Moso Oscuro"}</button>
+    </main>
   )
 }
 
